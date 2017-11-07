e9e8519f902bfc553e92196cb742de9bf4050dbc
obedgm <obedgm33@gmail.com>
2017-11-07 14:55:48 -0600

    First commit

---
+++ CONFIG.php
@@ -0,0 +1,8 @@
+<?php
+
+//SITE GLOBAL CONFIGURATION
+
+$email = "hola@andrearodriguezzavala.com"; 							//<-- Your email address
+$subscribe_email = "hola@andrearodriguezzavala.com"; 					//<<-- Email address for sending you subscribe notification
+$subscribe_notification_subject = "Nuevo suscriptor"; 	//<<-- Email subscribe notification subject
+?>
